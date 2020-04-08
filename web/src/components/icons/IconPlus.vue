578e64d2ef144b62ebda97a91f8c046281ca3668
Adi Utama <neogatotkaca@gmail.com>
2020-04-08 17:15:53 +0700

    Create icon components

---
+++ web/src/components/icons/IconPlus.vue
@@ -0,0 +1,14 @@
+<template>
+  <g>
+    <path d="M16 8.4c-0.663 0-1.2 0.537-1.2 1.2v0 12.8c0 0.663 0.537 1.2 1.2 1.2v0c0.663 0 1.2-0.537 1.2-1.2v0-12.8c0-0.663-0.537-1.2-1.2-1.2v0z"></path>
+    <path d="M9.6 14.8c-0.663 0-1.2 0.537-1.2 1.2v0c0 0.663 0.537 1.2 1.2 1.2v0h12.8c0.663 0 1.2-0.537 1.2-1.2v0c0-0.663-0.537-1.2-1.2-1.2v0h-12.8z"></path>
+  </g>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IconPlus'
+})
+</script>
