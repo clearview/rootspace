d80de3a11ce5ded4c234b44b4853a5e7fb234194
Adi Utama <neogatotkaca@gmail.com>
2020-04-14 15:57:54 +0700

    Add modal component

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="relative">
+    <router-view />
+    <portal-target name="default" />
   </div>
 </template>
 
---
+++ web/src/components/Modal.vue
@@ -0,0 +1,66 @@
+<template>
+  <portal
+    to="default"
+    v-if="visible"
+  >
+    <div class="modal">
+      <div
+        class="modal-background"
+        @click="close"
+      />
+
+      <div class="modal-inner">
+        <slot name="header">
+          <div class="modal-header">
+            <div class="modal-title">{{ title }}</div>
+            <button
+              class="btn btn-icon rounded-full"
+              @click="close"
+            >
+              <v-icon name="close" />
+            </button>
+          </div>
+        </slot>
+
+        <slot />
+
+        <slot name="footer">
+          <div class="modal-footer">
+            <button
+              class="btn btn-small"
+              @click="close"
+            >Cancel</button>
+            <button class="btn btn-small btn-primary">Save</button>
+          </div>
+        </slot>
+      </div>
+    </div>
+  </portal>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import VIcon from '@/components/icons/Index.vue'
+
+export default Vue.extend({
+  name: 'Modal',
+  inheritAttrs: false,
+  components: {
+    VIcon
+  },
+  props: {
+    visible: {
+      type: Boolean
+    },
+    title: {
+      type: String
+    }
+  },
+  methods: {
+    close (): void {
+      this.$emit('close')
+    }
+  }
+})
+</script>
