33aa29f8cdba8e79b263476ffccfc597584de6d7
Adi Utama <neogatotkaca@gmail.com>
2020-04-16 15:31:34 +0700

    Adding a utility for api request

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,35 @@
 <template>
-  <div id="app" class="relative">
+  <div
+    id="app"
+    class="relative"
+  >
     <router-view />
     <portal-target name="default" />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import api from '@/utils/api'
+
+export default Vue.extend({
+  created () {
+    this.updateAuthorization(this.$store.state.auth.token)
+
+    this.$store.watch(
+      ({ auth }) => auth.token,
+      (token) => {
+        this.updateAuthorization(token)
+      }
+    )
+  },
+  methods: {
+    updateAuthorization (token: string) {
+      api.defaults.headers.common.Authorization = `Bearer ${token}`
+    }
+  }
+})
+</script>
 
 <style src="@/assets/css/index.css" />
