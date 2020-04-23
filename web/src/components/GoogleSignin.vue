76b2930e7b47a60c65815c9d39ca2fcaef21f963
Mirza Eka <mirzaeka@gmail.com>
2020-04-23 13:08:22 +0700

    Create Google Signin Component

---
+++ web/src/components/GoogleSignin.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="btn w-full mx-0" type="button" v-on:click="authWithGoogle()">
+    <span class="mr-1">
+      <v-icon name="google" size="1.1em" />
+    </span>
+    Sign up with Google
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import VIcon from '@/components/icons/Index.vue'
+
+export default Vue.extend({
+  name: 'ButtonLock',
+  components: {
+    VIcon
+  },
+  methods: {
+    authWithGoogle () {
+      const API: string = process.env.VUE_APP_API_URL
+      location.href = `${API}/auth/google`
+    }
+  }
+})
+</script>
