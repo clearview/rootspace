27666391a6aaa8f69a26449203488d300dd1126b
ihti <jasminihtijarevic@gmail.com>
2020-04-28 04:18:30 +0200

    Doc entity

---
+++ api/src/entities/Doc.ts
@@ -0,0 +1,35 @@
+import {Entity, PrimaryGeneratedColumn, Column, Index, CreateDateColumn, UpdateDateColumn} from 'typeorm'
+
+@Entity('docs')
+export class Doc {
+
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column('integer', { nullable: true })
+  @Index()
+  userId: number
+
+  @Column('integer')
+  @Index()
+  spaceId: number
+
+  @Column('varchar')
+  title: string
+
+  @Column('text')
+  content: string
+
+  @Column('integer')
+  access: number
+
+  @Column('integer', { default: 0 })
+  revision: number
+
+  @CreateDateColumn()
+  created: string
+
+  @UpdateDateColumn()
+  updated: string
+
+}
