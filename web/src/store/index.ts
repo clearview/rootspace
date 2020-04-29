06545eebc93f42de6700bda999695c6d4c1d564d
Adi Utama <neogatotkaca@gmail.com>
2020-04-29 08:32:21 +0700

    Create nav module on store and persist it on localStorage

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -11,13 +11,15 @@ import local from '@/store/plugins/local'
 // Modules
 import auth from '@/store/modules/auth'
 import link from '@/store/modules/link'
+import nav from '@/store/modules/nav'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store<RootState>({
   modules: {
     auth,
-    link
+    link,
+    nav
   },
   plugins: [
     session.plugin,
--- web/src/store/plugins/local.ts
+++ web/src/store/plugins/local.ts
@@ -5,8 +5,10 @@ import { RootState } from '@/types/state'
 const vuexSession = new VuexPersist<RootState>({
   key: 'root_data',
   storage: window.localStorage,
-  reducer () {
-    return {}
+  reducer (state) {
+    return {
+      nav: state.nav
+    }
   }
 })
 
