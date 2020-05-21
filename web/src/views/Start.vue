79a5fe2efb76d3046c1d2bdd20a8d9dd12e0c3d7
Mirza Eka <mirzaeka@gmail.com>
2020-05-22 05:25:53 +0700

    WIP: Invitation - rethinking the flow

---
+++ web/src/views/Start.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main-container">
+    <v-alert v-model="notif" />
+    <h1>Get Started</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+
+    <h2 class="subtitle">Introduction</h2>
+    <div class="box"></div>
+
+    <h2 class="subtitle">Title</h2>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import VAlert from '@/components/Alert.vue'
+
+export default Vue.extend({
+  name: 'Start',
+  components: {
+    VAlert
+  },
+  mounted () {
+    const query = this.$route.query
+    console.log(query)
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+p {
+  @apply text-base;
+  color: theme("colors.gray.400");
+}
+
+.subtitle {
+  @apply font-bold text-base mt-8;
+}
+
+.box {
+  @apply w-full h-64 mt-4;
+
+  background-color: theme("colors.gray.100");
+
+}
+</style>
