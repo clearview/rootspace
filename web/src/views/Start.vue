a152e2b5855367d604c5083f17bdd1deaef9c5b6
Mirza Eka <mirzaeka@gmail.com>
2020-05-29 07:24:46 +0700

    Alert invited new space

--- web/src/views/Start.vue
+++ web/src/views/Start.vue
@@ -1,31 +1,62 @@
 <template>
-  <div class="main-container">
-    <v-alert v-model="notif" />
-    <h1>Get Started</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+  <div>
+    <v-alert v-model="alert" />
+    <div class="main-container">
+      <h1>Get Started</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
 
-    <h2 class="subtitle">Introduction</h2>
-    <div class="box"></div>
+      <h2 class="subtitle">Introduction</h2>
+      <div class="box"></div>
 
-    <h2 class="subtitle">Title</h2>
-    <div class="box"></div>
+      <h2 class="subtitle">Title</h2>
+      <div class="box"></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { isEmpty } from 'lodash/fp'
 
 import VAlert from '@/components/Alert.vue'
+
+type ComponentData = {
+  alert: object | null;
+}
 
 export default Vue.extend({
   name: 'Start',
   components: {
     VAlert
   },
-  mounted () {
+  data (): ComponentData {
+    return {
+      alert: null
+    }
+  },
+  created () {
     const query = this.$route.query
     console.log(query)
+    if (query.from === 'invitation' && query.accept === '1') {
+      this.alert = {
+        type: 'success',
+        message: `Welcome to ${this.currentSpace.title}, you are invited to this space`,
+        noicon: true
+      }
+    }
+  },
+  watch: {
+    $route (newval) {
+      if (isEmpty(newval.query)) {
+        this.alert = null
+      }
+    }
+  },
+  computed: {
+    currentSpace () {
+      return this.$store.state.auth.currentSpace || {}
+    }
   }
 })
 </script>
@@ -44,6 +75,5 @@ p {
   @apply w-full h-64 mt-4;
 
   background-color: theme("colors.gray.100");
-
 }
 </style>
