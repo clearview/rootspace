f90e8a5c3555a452b85e3bc97976492faf75548a
ihti <jasminihtijarevic@gmail.com>
2020-06-01 10:54:47 +0200

    Fix auth

--- api/src/passport.ts
+++ api/src/passport.ts
@@ -37,6 +37,7 @@ passport.use(
           firstName: profile.name.givenName,
           lastName: profile.name.familyName,
           email: profile.emails[0].value,
+          emailConfirmed: true,
           password: '',
           authProvider: 'google',
           active: true,
