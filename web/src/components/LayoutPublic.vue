d45c49995b00831c8ba2d98d60172abf0da7b204
Adi Utama <neogatotkaca@gmail.com>
2020-06-03 01:22:29 +0700

    Create layout public component

---
+++ web/src/components/LayoutPublic.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layout public">
+    <header class="header">
+      <img
+        class="object-contain"
+        alt="Root Logo"
+        src="@/assets/images/logo-inverse.png"
+        srcset="
+          @/assets/images/logo-inverse.png,
+          @/assets/images/logo-inverse@2x.png 2x
+        "
+      />
+      <h1 class="pl-2">RootApp</h1>
+    </header>
+
+    <div class="content">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LayoutPublic'
+})
+</script>
