18bb83a657c80e3f1783515d53e86e91469c55dd
Aid Arslanagic <aid@clearview.team>
2020-06-09 21:25:48 +0200

    Removing old router file, adding jest db helper

--- api/tests/express.test.ts
+++ api/tests/express.test.ts
@@ -1,17 +1,20 @@
 import request from 'supertest'
 import Server from '../src/server'
+import {spawnTestDatabase, destroyTestDatabase} from './helpers/db'
 
 const server = new Server()
 
 describe('Express', () => {
 
     beforeAll(async () => {
+        await spawnTestDatabase()
         await server.bootstrap()
-        server.listen()
+        server.listen(3333)
     })
 
     afterAll(async () => {
         await server.close()
+        await destroyTestDatabase()
     })
 
     it('should GET root API', async () => {
---
+++ api/tests/helpers/db.ts
@@ -0,0 +1,37 @@
+import {Connection, getConnection, getConnectionManager} from 'typeorm'
+import {GenericContainer, StartedTestContainer} from 'testcontainers'
+
+let container: StartedTestContainer = null
+let connection: Connection
+
+async function spawnTestDatabase () {
+    container = await new GenericContainer('postgres', '12')
+        .withName('root_test')
+        .withEnv('POSTGRES_USER', 'test')
+        .withEnv('POSTGRES_PASSWORD', 'test')
+        .withEnv('POSTGRES_DB', 'test')
+        .withExposedPorts(5432)
+        .start()
+
+    const connectionManager = getConnectionManager()
+
+    connection = connectionManager.create({
+        name: 'default',
+        type: 'postgres',
+        url: `postgresql://test:test@${container.getContainerIpAddress()}:${container.getMappedPort(5432)}/test`,
+        logging: false,
+        synchronize: true,
+        migrations: [`${__dirname}/../src/migrations/*{.ts,.js}`],
+        entities: [`${__dirname}/../src/entities/*{.ts,.js}`],
+        subscribers: [`${__dirname}/../src/entities/subscribers/*{.ts,.js}`]
+    })
+
+    await connection.connect()
+}
+
+async function destroyTestDatabase() {
+    await getConnection().close()
+    await container.stop()
+}
+
+export {spawnTestDatabase, destroyTestDatabase}
