f5df9b19c8b78904fe047e75e8659d83eb6da410
Mujo Kodro <mujo.kodro@gmail.com>
2020-06-10 13:58:28 +0200

    link position after insert into subscriber

--- api/src/entities/subscribers/LinkSubscriber.ts
+++ api/src/entities/subscribers/LinkSubscriber.ts
@@ -1,9 +1,26 @@
-import { EntitySubscriberInterface, EventSubscriber } from 'typeorm'
+import {
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+  getCustomRepository,
+} from 'typeorm'
 import { Link } from '../Link'
+import { LinkRepository } from '../../repositories/LinkRepository'
 
 @EventSubscriber()
 export class LinkSubscriber implements EntitySubscriberInterface<Link> {
   listenTo() {
     return Link
+  }
+
+  async beforeInsert(event: InsertEvent<Link>) {
+    const link = event.entity
+
+    if (link.parent) {
+      link.position =
+        (await getCustomRepository(LinkRepository).getNodeMaxPosition(
+          link.parent.id
+        )) + 1
+    }
   }
 }
