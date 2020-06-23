c5745be4c6fe6bc672b95b317ab606dec0ffb216
Aid Arslanagic <aid@clearview.team>
2020-06-23 19:54:15 +0200

    ACL

--- api/src/middleware/AuthorizationMiddleware.ts
+++ api/src/middleware/AuthorizationMiddleware.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import {Ability, AbilityBuilder} from '@casl/ability'
+import {SpaceService} from '../services'
+import {User} from '../entities/User'
 
 export enum Actions {
     'Create'= 'create',
@@ -10,34 +12,57 @@ export enum Actions {
 }
 
 export enum Objects {
-    'Link'= 'Link'
+    'All' = 'all',
+    'Doc' = 'Doc',
+    'Link' = 'Link',
+    'TaskBoard' = 'TaskBoard'
 }
 
-// export function authorize (req: Request, res: Response, next: NextFunction) {
-//     // tslint:disable-next-line:no-console
-//     console.log('Time:', Date.now())
-//
-//     if (!req.user) {
-//         // tslint:disable-next-line:no-console
-//         console.log('NO USER')
-//     }
-//
-//     // tslint:disable-next-line:no-console
-//     console.log('User:', req.user.id)
-//
-//     next()
-// }
+interface AuthorizationInfo { user: User, userSpaceIds: number[] }
 
-export function authorize (action: Actions, object: Objects) {
+export function authorize (object: Objects, action?: Actions) {
     return async (req: Request, res: Response, next: NextFunction) => {
         const { can, cannot, rules } = new AbilityBuilder()
 
         if (!req.user) { next() }
 
-        can(action, [object], { userId: req.user.id })
+        const authorizationInfo = await getAuthorizationInfo(req)
+
+        await applyGeneralRules(authorizationInfo, can, cannot)
+        await applyClassSpecificRules(authorizationInfo, can, cannot, rules)
 
         req.user.ability = new Ability(rules)
 
         next()
+    }
+
+    async function getAuthorizationInfo(req: Request): Promise<AuthorizationInfo> {
+        const userSpaces = await SpaceService.getInstance().getSpacesByUserId(req.user.id)
+        const userSpaceIds = userSpaces.map(space => { return space.id })
+
+        return {
+            user: req.user,
+            userSpaceIds
+        }
+    }
+
+    async function applyGeneralRules(authorizationInfo: AuthorizationInfo, can, cannot) {
+        // User can manage any object they own
+        can(Actions.Manage, [object], { userId: authorizationInfo.user.id })
+
+        // User can read any object from the space they have access to
+        can(Actions.Read, [object], { spaceId: { $in: authorizationInfo.userSpaceIds } })
+
+        // User can not create objects outside spaces they belong to
+        cannot(Actions.Create, [object], { spaceId: { $nin: authorizationInfo.userSpaceIds } })
+    }
+
+    async function applyClassSpecificRules(authorizationInfo: AuthorizationInfo, can, cannot, rules) {
+        switch (object) {
+            case Objects.Doc:
+                break
+            case Objects.TaskBoard:
+                break
+        }
     }
 }
