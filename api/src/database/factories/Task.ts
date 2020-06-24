fcf49a30a59b4015e5a28f8cb67bf259b502f667
Aid Arslanagic <aid@clearview.team>
2020-06-25 01:43:24 +0200

    Seeder refactoring + factories

--- api/src/database/factories/Task.ts
+++ api/src/database/factories/Task.ts
@@ -1,21 +1,21 @@
 import * as Faker from 'faker'
-import { define } from 'typeorm-seeding'
+import { define, factory } from 'typeorm-seeding'
 import { Task, TaskStatus } from '../../entities/tasks/Task'
+import { User } from '../../entities/User'
+import { Space } from '../../entities/Space'
+import { TaskList } from '../../entities/tasks/TaskList'
 
-define(Task, (faker: typeof Faker) => {
-
-  const tags = []
-
-  for(let i = 0; i < faker.random.number(3); i++){
-    tags.push(faker.hacker.noun())
-  }
-
+define(Task, (faker: typeof Faker, context: any) => {
   const task = new Task()
   task.title = faker.lorem.lines(1)
   task.description = faker.lorem.sentence()
   task.status = faker.random.boolean() === true ? TaskStatus.Closed : TaskStatus.Open
-  task.tags = tags
   task.dueDate = faker.random.boolean() === true ? faker.date.future(1) : null
+  task.user = context.user ? context.user : factory(User)() as any
+  task.space = context.space ? context.space : factory(Space)() as any
+  task.list = context.list ? context.list : factory(TaskList)() as any
+
+  task.tags = []
 
   return task
 })
--- api/src/database/factories/TaskList.ts
+++ api/src/database/factories/TaskList.ts
@@ -1,12 +1,18 @@
 import * as Faker from 'faker'
-import { define, times } from 'typeorm-seeding'
+import { define, factory, times } from 'typeorm-seeding'
 import { TaskList } from '../../entities/tasks/TaskList'
+import { TaskBoard } from '../../entities/tasks/TaskBoard'
+import { User } from '../../entities/User'
+import { Space } from '../../entities/Space'
 
-define(TaskList, (faker: typeof Faker) => {
+define(TaskList, (faker: typeof Faker, context: any) => {
 
   const taskList = new TaskList()
   taskList.title = faker.name.jobTitle()
   taskList.description = faker.name.jobDescriptor()
+  taskList.user = context.user ? context.user : factory(User)() as any
+  taskList.space = context.space ? context.space : factory(Space)() as any
+  taskList.board = context.board ? context.board : factory(TaskBoard)() as any
 
   return taskList
 })
