efa02df5acec0fdd0ae69dc805382132ce55fa97
Aid Arslanagic <aid@clearview.team>
2020-06-25 04:58:32 +0200

    Tag entity

--- api/src/entities/tasks/Tag.ts
+++ api/src/entities/tasks/Tag.ts
@@ -3,26 +3,22 @@ import {
   PrimaryGeneratedColumn,
   Column,
   Index,
-  ManyToOne,
+  ManyToOne, JoinColumn,
 } from 'typeorm'
 import { TaskBoard } from './TaskBoard'
 
 @Entity('task_board_tags')
 export class Tag {
-
   @PrimaryGeneratedColumn()
   id: number
 
-  @Column('integer')
-  @Index()
-  boardId: number
+  @ManyToOne(type => TaskBoard, board => board.tags, {onDelete: 'CASCADE'})
+  @JoinColumn({ name: 'boardId' })
+  @Index()  board!: TaskBoard
 
   @Column('varchar')
   label: string
 
   @Column('varchar')
   color: string
-
-  @ManyToOne(type => TaskBoard, board => board.tags, {onDelete: 'CASCADE'})
-  board!: TaskBoard
 }
