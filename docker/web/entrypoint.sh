092c8e3c1abe69c2848bc3a51b272861d5a96abd
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2020-06-30 09:40:52 +0800

    Nomad - splot PG and Proxy jobs

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
-cd /srv/member && \
+cd /srv/ && \
+pwd && \
+ls . && \
 echo "Installing packages... $(yarn install)"
-echo "Building member...  $(yarn build)"
-cd /srv/org && \
-echo "Installing packages... $(yarn install)"
-echo "Building org...  $(yarn build)"
+echo "Building web...  $(yarn build)"
 exec "$@"
