8738228f8a6b67b1345309d2bf43c72acd6d1688
Mirza Eka <mirzaeka@gmail.com>
2020-07-04 17:05:55 +0700

    Tweak ReadOnly Document Component

--- web/src/components/editor/ReadOnly.vue
+++ web/src/components/editor/ReadOnly.vue
@@ -3,20 +3,7 @@
     <div class="codex-editor">
       <div class="ce-block" v-for="(val, index) in value.blocks" :key="index">
         <div class="ce-block__content">
-          <editor-checklist v-if="val.type === 'checklist'" :value="val"></editor-checklist>
-          <editor-code v-if="val.type === 'code'" :value="val"></editor-code>
-          <editor-delimiter v-if="val.type === 'delimiter'" :value="val"></editor-delimiter>
-          <editor-h1 v-if="val.type === 'h1'" :value="val"></editor-h1>
-          <editor-h2 v-if="val.type === 'h2'" :value="val"></editor-h2>
-          <editor-h3 v-if="val.type === 'h3'" :value="val"></editor-h3>
-          <editor-image v-if="val.type === 'image'" :value="val"></editor-image>
-          <editor-link-tool v-if="val.type === 'linkTool'" :value="val"></editor-link-tool>
-          <editor-list v-if="val.type === 'list'" :value="val"></editor-list>
-          <editor-paragraph v-if="val.type === 'paragraph'" :value="val"></editor-paragraph>
-          <editor-quote v-if="val.type === 'quote'" :value="val"></editor-quote>
-          <editor-raw v-if="val.type === 'raw'" :value="val"></editor-raw>
-          <editor-table v-if="val.type === 'table'" :value="val"></editor-table>
-          <editor-warning v-if="val.type === 'warning'" :value="val"></editor-warning>
+          <component :is="editorReadonlyComponent(val.type)" :value="val"></component>
         </div>
       </div>
     </div>
@@ -66,5 +53,51 @@ import EditorWarning from '@/components/editor/readonlyMode/Warning.vue'
 export default class ReadOnly extends Vue {
   @Prop({ type: Object })
   private readonly value!: DocumentResource;
+
+  editorReadonlyComponent (type: string) {
+    switch (type) {
+      case 'checklist':
+        return 'editor-checklist'
+
+      case 'code':
+        return 'editor-code'
+
+      case 'delimiter':
+        return 'editor-delimiter'
+
+      case 'h1':
+        return 'editor-h1'
+
+      case 'h2':
+        return 'editor-h2'
+
+      case 'h3':
+        return 'editor-h3'
+
+      case 'image':
+        return 'editor-image'
+
+      case 'linkTool':
+        return 'editor-link-tool'
+
+      case 'list':
+        return 'editor-list'
+
+      case 'paragraph':
+        return 'editor-paragraph'
+
+      case 'quote':
+        return 'editor-quote'
+
+      case 'raw':
+        return 'editor-raw'
+
+      case 'table':
+        return 'editor-table'
+
+      case 'warning':
+        return 'editor-warning'
+    }
+  }
 }
 </script>
