f46e0e73133ca8824d08eeb46fa51571414b371c
Aid Arslanagic <aid@clearview.team>
2020-07-11 14:56:01 +0200

    Seeder fix

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -7,8 +7,6 @@ module.exports = {
   logging: false,
   synchronize: false,
   migrationsRun: true,
-  seeds: ['src/database/seeder/seeds/**/*{.ts,.js}'],
-  factories: ['src/database/seeder/seeds/factories/**/*{.ts,.js}'],
   entities: [`${__dirname}/src/database/entities/**/*{.ts,.js}`],
   subscribers: [`${__dirname}/src/database/subscribers/**/*{.ts,.js}`],
   migrations: [`${__dirname}/src/database/migrations/**/*{.ts,.js}`],
