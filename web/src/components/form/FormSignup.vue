65e3e4140f2afd46921d71f5d6407a3cb952bf4c
Adi Utama <neogatotkaca@gmail.com>
2020-07-14 16:17:06 +0700

    Update signup form

--- web/src/components/form/FormSignup.vue
+++ web/src/components/form/FormSignup.vue
@@ -1,134 +1,163 @@
 <template>
-  <form
-    class="mt-10"
-    @submit.prevent="submit"
-  >
-    <v-field label="First Name" name="firstname">
-      <div class="form-group">
-        <input
-          class="input w-full leading-tight mx-0"
-          id="firstName"
-          type="text"
-          placeholder="Enter your first name"
-          v-model.trim="$v.payload.firstName.$model"
-        />
-        <span class="icon">
-          <v-icon name="user" size="1.5em"/>
-        </span>
-      </div>
-      <div class="error-group">
-        <div
-          class="error"
-          v-if="$v.payload.firstName.$dirty && !$v.payload.firstName.required"
-        >First Name is required.
-        </div>
-      </div>
+  <form class="form" @submit.prevent="submit">
+    <v-field
+      label="First Name"
+      name="firstname"
+      has-icon-right
+    >
+      <input
+        class="input"
+        id="firstName"
+        type="text"
+        placeholder="Enter your first name"
+        v-model.lazy.trim="$v.payload.firstName.$model"
+      />
+      <v-icon
+        class="icon is-right"
+        name="user"
+        size="1.5em"
+      />
+
+      <template #help v-if="$v.payload.firstName.$error">
+        <p
+          v-if="!$v.payload.firstName.required"
+          class="help is-danger"
+        >
+          First Name is required.
+        </p>
+      </template>
     </v-field>
 
-    <v-field label="Last Name" name="lastName">
-      <div class="form-group">
-        <input
-          class="input w-full leading-tight mx-0"
-          id="lastName"
-          type="text"
-          placeholder="Enter your last name"
-          v-model.trim="$v.payload.lastName.$model"
-        />
-        <span class="icon">
-          <v-icon name="user" size="1.5em"/>
-        </span>
-      </div>
-      <div class="error-group">
+    <v-field
+      label="Last Name"
+      name="lastName"
+      has-icon-right
+    >
+      <input
+        class="input"
+        id="lastName"
+        type="text"
+        placeholder="Enter your last name"
+        v-model.lazy.trim="$v.payload.lastName.$model"
+      />
+      <v-icon
+        class="icon is-right"
+        name="user"
+        size="1.5em"
+      />
+
+      <template #help v-if="$v.payload.lastName.$error">
         <div
-          class="error"
-          v-if="$v.payload.lastName.$dirty && !$v.payload.lastName.required"
-        >Last Name is required.
+          v-if="!$v.payload.lastName.required"
+          class="help is-danger"
+        >
+          Last Name is required.
         </div>
-      </div>
+      </template>
     </v-field>
 
-    <v-field label="Email" name="email">
-      <div class="form-group">
-        <input
-          class="input w-full leading-tight mx-0"
-          id="email"
-          type="text"
-          placeholder="Enter your email"
-          v-model.trim="$v.payload.email.$model"
-        />
-        <span class="icon">
-          <v-icon name="email" size="1.5em"/>
-        </span>
-      </div>
-      <div class="error-group">
-        <div
-          class="error"
-          v-if="$v.payload.email.$dirty && !$v.payload.email.required"
-        >Email is required.
-        </div>
-        <div
-          class="error"
-          v-if="$v.payload.email.$dirty && !$v.payload.email.email"
-        >Email format is not valid.
-        </div>
-      </div>
+    <v-field
+      label="Email"
+      name="email"
+    >
+      <input
+        class="input"
+        id="email"
+        type="text"
+        placeholder="Enter your email"
+        v-model.lazy.trim="$v.payload.email.$model"
+      />
+      <v-icon
+        class="icon is-right"
+        name="email"
+        size="1.5em"
+      />
+
+      <template #help v-if="$v.payload.email.$error">
+        <p
+          v-if="!$v.payload.email.required"
+          class="help is-danger"
+        >
+          Email is required.
+        </p>
+        <p
+          v-if="!$v.payload.email.email"
+          class="help is-danger"
+        >
+          Email format is not valid.
+        </p>
+      </template>
     </v-field>
 
-    <v-field label="Password" name="password">
-      <div class="form-group">
-        <input
-          class="input w-full leading-tight mx-0"
-          id="password"
-          type="password"
-          placeholder="******"
-          v-model.trim="$v.payload.password.$model"
+    <v-field
+      label="Password"
+      name="password"
+      has-icon-right
+    >
+      <input
+        class="input"
+        id="password"
+        type="password"
+        placeholder="Enter your password"
+        v-model.trim="$v.payload.password.$model"
+      />
+      <span class="icon is-right">
+        <v-icon
+          name="lock"
+          size="1.5em"
         />
-        <span class="icon">
-          <v-icon name="lock" size="1.5em"/>
-        </span>
-      </div>
-      <div class="error-group">
+      </span>
+
+      <template #help v-if="$v.payload.password.$dirty">
+        <p
+          v-if="!$v.payload.password.required"
+          class="help is-danger"
+        >
+          Password is required.
+        </p>
         <div
-          class="error"
-          v-if="$v.payload.password.$dirty && !$v.payload.password.required"
-        >Password is required.
+          v-if="!$v.payload.password.minLength"
+          class="help is-danger"
+        >
+          Password must have at least {{ $v.payload.password.$params.minLength.min }} letters.
         </div>
-        <div
-          class="error"
-          v-if="$v.payload.password.$dirty && !$v.payload.password.minLength"
-        >Password must have at least {{ $v.payload.password.$params.minLength.min }} letters.
-        </div>
-      </div>
+      </template>
     </v-field>
 
-    <v-field label="Repeat Password" name="repeatpassword">
-      <div class="form-group">
-        <input
-          class="input w-full leading-tight mx-0"
-          id="repeatpassword"
-          type="password"
-          placeholder="******"
-          v-model.trim="$v.payload.password_confirmation.$model"
-        />
-        <span class="icon">
-          <v-icon name="lock" size="1.5em"/>
-        </span>
-      </div>
-      <div class="error-group">
-        <div
-          class="error"
+    <v-field
+      label="Repeat Password"
+      name="repeatpassword"
+      has-icon-right
+    >
+      <input
+        class="input"
+        id="repeatpassword"
+        type="password"
+        placeholder="Enter your password again"
+        v-model.trim="$v.payload.password_confirmation.$model"
+      />
+      <v-icon
+        class="icon is-right"
+        name="lock"
+        size="1.5em"
+      />
+
+      <template #help v-if="$v.payload.password_confirmation.$error">
+        <p
           v-if="!$v.payload.password_confirmation.sameAsPassword"
-        >Passwords must be identical.
-        </div>
-      </div>
+          class="help is-danger"
+        >
+          Passwords must be identical.
+        </p>
+      </template>
     </v-field>
 
     <button
       class="btn btn-primary w-full mx-0 mt-5"
-      type="button"
+      type="submit"
       :disabled="$v.payload.$invalid"
-      @click="submit()"
-    >Sign Up
+    >
+      Sign Up
     </button>
   </form>
 </template>
@@ -164,20 +193,20 @@ import { Component, Vue } from 'vue-property-decorator'
   }
 })
 export default class FormSignup extends Vue {
-    private payload: SignupResource = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      password_confirmation: '' // eslint-disable-line
-    }
+  private payload: SignupResource = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    password_confirmation: '' // eslint-disable-line
+  }
 
-    submit (): void {
-      this.$v.payload.$touch()
+  submit (): void {
+    this.$v.payload.$touch()
 
-      if (!this.$v.payload.$invalid) {
-        this.$emit('submit', this.payload)
-      }
+    if (!this.$v.payload.$invalid) {
+      this.$emit('submit', this.payload)
     }
+  }
 }
 </script>
