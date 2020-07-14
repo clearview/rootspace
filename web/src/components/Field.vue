d05da63ed4b442f14d505f6d636deb1cc84b094c
Adi Utama <neogatotkaca@gmail.com>
2020-07-14 16:16:17 +0700

    Improve field component style

--- web/src/components/Field.vue
+++ web/src/components/Field.vue
@@ -1,15 +1,31 @@
 <template>
-  <div :class="['field', {
-    'field-inline': inline,
-    'field-border': border,
-    'field-disabled': disabled
-  }]">
+  <div
+    class="field"
+    :class="{
+      'is-inline': inline,
+      'is-bordered': border,
+      'is-disabled': disabled
+    }"
+  >
     <label
-      class="field-label"
+      v-if="label"
       v-text="label"
+      class="label"
       :for="name"
     />
-    <slot />
+    <div
+      class="control"
+      :class="{
+        'has-icon-left': hasIconLeft,
+        'has-icon-right': hasIconRight,
+        'justify-start': align === 'left',
+        'justify-end': align === 'right'
+      }"
+    >
+      <slot />
+    </div>
+
+    <slot name="help" />
   </div>
 </template>
 
@@ -25,7 +41,7 @@ export default class Field extends Vue {
   private readonly label?: string;
 
   @Prop({ type: String, default: '' })
-  private readonly name!: string;
+  private readonly name?: string;
 
   @Prop({ type: Boolean })
   private readonly inline?: boolean;
@@ -35,5 +51,14 @@ export default class Field extends Vue {
 
   @Prop({ type: Boolean })
   private readonly disabled?: boolean;
+
+  @Prop({ type: String })
+  private readonly align?: string;
+
+  @Prop({ type: Boolean })
+  private readonly hasIconLeft?: boolean;
+
+  @Prop({ type: Boolean })
+  private readonly hasIconRight?: boolean;
 }
 </script>
