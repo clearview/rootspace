095c615eaba2a6f1b0d9d925c9a8a5f0c8b48e0b
Aditya Purwa <adityapurwa@windowslive.com>
2020-07-17 14:08:30 +0700

    rework vuex and network calls related with task

---
+++ web/src/store/modules/task/board.ts
@@ -0,0 +1,25 @@
+import { createServiceModule } from '@/store/utils/createServiceModule'
+import { BoardService } from '@/services/task'
+import api from '@/utils/api'
+
+const board = createServiceModule(BoardService)
+if (board.actions) {
+  board.actions.search = async ({ commit }, params: {
+    boardId: number;
+    search: string;
+    filters: {
+      tags: number[];
+      assignees: number[];
+    };
+  }) => {
+    commit('setFetching', true)
+    const res = await api.post(`tasks/board/${params.boardId}/search`, {
+      data: { search: params.search, filters: params.filters }
+    })
+    commit('setFetching', false)
+    commit('setCurrent', res?.data.data)
+    return res
+  }
+}
+
+export default board
