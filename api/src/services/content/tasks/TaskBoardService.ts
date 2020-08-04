99efb3f7e6644ca584c11adb99b6c860ad08f437
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-04 21:02:04 +0200

    node task board title sync

--- api/src/services/content/tasks/TaskBoardService.ts
+++ api/src/services/content/tasks/TaskBoardService.ts
@@ -10,6 +10,7 @@ import { NodeService } from '../NodeService'
 import { NodeType } from '../../../types/node'
 import { NodeCreateValue } from '../../../values/node'
 import { Task } from '../../../database/entities/tasks/Task'
+import { INodeContentUpdate } from '../contracts'
 import { ActivityService } from '../../ActivityService'
 import Bull from 'bull'
 import { ActivityEvent } from '../../events/ActivityEvent'
@@ -107,6 +108,7 @@ export class TaskBoardService extends NodeContentService {
 
   async update(id: number, data: any): Promise<TaskBoard> {
     let taskBoard = await this.getById(id)
+
     taskBoard = await this.getTaskBoardRepository().save({
       ...taskBoard,
       ...data,
@@ -126,6 +128,24 @@ export class TaskBoardService extends NodeContentService {
     await this.nodeContentMediator.contentRemoved(id, this.getNodeType())
 
     return taskBoard
+  }
+
+  async nodeUpdated(
+    contentId: number,
+    data: INodeContentUpdate
+  ): Promise<void> {
+    if (!data.title) {
+      return
+    }
+
+    const taskBoard = await this.getById(contentId)
+
+    if (!taskBoard) {
+      return
+    }
+
+    taskBoard.title = data.title
+    await this.getTaskBoardRepository().save(taskBoard)
   }
 
   async nodeRemoved(contentId: number): Promise<void> {
