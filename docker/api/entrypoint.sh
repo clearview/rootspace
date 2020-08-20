98402403ff9aaf3c51b25e43db16cc4110512fff
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2020-08-20 11:49:26 +0800

    remove consul Dockerfile

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
-if test -f "$NOMAD_TASK_DIR/.env" ; then
-    cp $NOMAD_TASK_DIR/.env /srv/api/.env
-fi
-
 cd /srv/api && \
 printenv | sort && \
-cat /srv/api/.env && \
 echo "Installing packages... $(yarn install --production=false)"
 echo "Building... $(yarn build)"
 exec "$@"
