20c8e29df348ac4d99cd9826dea176a2412df5dc
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-23 22:41:49 +0200

    archive nodes set parent to null command

--- api/src/commands/nodes/index.ts
+++ api/src/commands/nodes/index.ts
@@ -1,15 +1,22 @@
 import { Command } from 'commander'
 import { PositionsCommand } from './PositionsCommand'
+import { ArchivesSetParentNullCommand } from './ArchivesSetParentNullCommand'
 
 export function nodeCommands() {
-  const commands = new Command('node')
-    .description('Node related commands')
+  const commands = new Command('node').description('Node related commands')
 
   commands
     .command('positions')
     .description('Re-set node positions')
     .action(async () => {
       await PositionsCommand.run()
+    })
+
+  commands
+    .command('archives-set-parent-null')
+    .description('Set archvie nodes parent to null')
+    .action(async () => {
+      await ArchivesSetParentNullCommand.run()
     })
 
   return commands
