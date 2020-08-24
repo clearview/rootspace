043fea2494a9792965a46c0d80dd47e53395f7f3
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-24 21:22:09 +0200

    uploads entity, entity id and contentType

--- api/src/controllers/UploadsCtrl.ts
+++ api/src/controllers/UploadsCtrl.ts
@@ -1,10 +1,10 @@
-import { NextFunction, Request, Response } from 'express'
-import { UploadService } from '../services/UploadService'
-import { UploadValidator } from '../validation/upload'
+import { Request, Response } from 'express'
 import { BaseCtrl } from './BaseCtrl'
+import { validateUpload } from '../validation/upload'
+import { UploadService } from '../services/UploadService'
+import { UploadCreateValue } from '../values/upload'
 
 export class UploadsCtrl extends BaseCtrl {
-
   private uploadService: UploadService
 
   constructor() {
@@ -12,20 +12,15 @@ export class UploadsCtrl extends BaseCtrl {
     this.uploadService = new UploadService()
   }
 
-  async index(req: Request, res: Response, next: NextFunction) {
-    try {
-      const data = {
-        spaceId: req.query.spaceId,
-        userId: req.user.id,
-        file: req.file
-      }
-      const validator = new UploadValidator()
-      await validator.validate(data)
+  async index(req: Request, res: Response) {
+    const data = req.body
+    const file = req.file
 
-      const upload = await this.uploadService.upload(req.file, data)
-      res.send(upload)
-    } catch (err) {
-      next(err)
-    }
+    await validateUpload(Object.assign({ ...data }, { file }))
+
+    const value = UploadCreateValue.fromObjectAndUserId(data, req.user.id).withFile(file)
+    const upload = await this.uploadService.create(value)
+
+    res.send(upload)
   }
 }
