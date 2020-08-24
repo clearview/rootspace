1707cf23cc06f9a64a974b62c120a83b204bf190
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-25 00:05:51 +0200

    uploads versions

---
+++ api/opt/project/src/database/migrations/1598303316992-UploadsAddEntityIdColumn.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UploadsAddEntityIdColumn1598303316992 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
