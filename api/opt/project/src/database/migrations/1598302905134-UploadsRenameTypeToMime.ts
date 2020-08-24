804bd25000dcf1247875b7387f6478ddedd8d80b
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-25 00:05:51 +0200

    uploads versions

---
+++ api/opt/project/src/database/migrations/1598302905134-UploadsRenameTypeToMime.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UploadsRenameTypeToMime1598302905134 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
