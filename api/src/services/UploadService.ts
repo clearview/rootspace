b68c8e1ef20c12555a027656d1efcc42922a11ab
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-26 01:20:44 +0200

    upload image versions

--- api/src/services/UploadService.ts
+++ api/src/services/UploadService.ts
@@ -1,18 +1,20 @@
 import httpRequestContext from 'http-request-context'
 import { config } from 'node-config-ts'
-import { getCustomRepository } from 'typeorm'
-import { UploadRepository } from '../database/repositories/UploadRepository'
-import { Upload } from '../database/entities/Upload'
 import { nanoid } from 'nanoid'
 import S3 from 'aws-sdk/clients/s3'
 import path from 'path'
 import fs from 'fs'
 import Bull from 'bull'
+import sharp from 'sharp'
+import { getCustomRepository } from 'typeorm'
+import { UploadRepository } from '../database/repositories/UploadRepository'
+import { Upload } from '../database/entities/Upload'
 import { ActivityEvent } from './events/ActivityEvent'
 import { ActivityService } from './ActivityService'
 import { FileActivities } from '../database/entities/activities/FileActivities'
 import { ServiceFactory } from './factory/ServiceFactory'
-import { UploadCreateValue } from '../values/upload'
+import { UploadValue } from '../values/upload'
+import { UploadType, UploadUniqueTypes, IUploadImageConfig, UploadImageConfig } from '../types/upload'
 
 export class UploadService {
   private s3: S3
@@ -35,21 +37,39 @@ export class UploadService {
     return this.getUploadRepository().findOne(id)
   }
 
-  async create(data: UploadCreateValue) {
-    const filePath = path.join(
-      String(data.attributes.spaceId),
-      nanoid(23),
-      data.file.originalname.replace(/\s+/g, '-').toLowerCase()
-    )
+  getUploadByEntityId(entityId: number, entity: string): Promise<Upload | undefined> {
+    return this.getUploadRepository().getByEntityId(entityId, entity)
+  }
 
-    const sFFile: any = await this.sendFileToS3(data.file, filePath)
+  async upload(data: UploadValue) {
+    const filePath = this.createFilePath(data.file.originalname, data.attributes.spaceId)
 
-    let upload = this.getUploadRepository().create()
+    const sFFile = await this.sendFileToS3({
+      Key: filePath,
+      Bucket: config.s3.bucket,
+      ACL: 'public-read',
+      ContentType: data.file.mimetype,
+      Body: fs.createReadStream(data.file.path),
+    })
+
+    const versions = await this.createImageVersions(data)
+
+    let upload = null
+
+    if (UploadUniqueTypes.includes(data.attributes.type)) {
+      upload = await this.getUploadByEntityId(data.attributes.entityId, data.attributes.entity)
+    }
+
+    if (!upload) {
+      upload = this.getUploadRepository().create()
+    }
+
     Object.assign(upload, data.attributes)
 
     upload.mime = data.file.mimetype
     upload.size = data.file.size
     upload.path = sFFile.Location
+    upload.versions = versions
 
     upload = await this.getUploadRepository().save(upload)
     await this.registerActivityForUpload(FileActivities.Uploaded, upload)
@@ -57,24 +77,79 @@ export class UploadService {
     return upload
   }
 
-  sendFileToS3(file: any, filePath: string) {
+  private createFilePath(fileName: string, spaceId: number) {
+    return path.join(String(spaceId), nanoid(23), fileName.replace(/\s+/g, '-').toLowerCase())
+  }
+
+  private async createImageVersions(data: UploadValue): Promise<object | null> {
+    const uploadType = data.attributes.type
+    const file = data.file
+
+    const cnfg = this.getUploadImageConfig(uploadType)
+
+    if (!cnfg) {
+      return null
+    }
+
+    const versions = {}
+    const images = []
+
+    for (const size of cnfg.sizes) {
+      await sharp(file.path)
+        .resize(size.width, size.height)
+        .toBuffer()
+        .then((output) => {
+          images[size.name] = output
+        })
+        .catch((err) => {
+          throw err
+        })
+    }
+
+    for (const size of cnfg.sizes) {
+      const originalFileName: string = file.originalname
+
+      const versionfileName = [
+        originalFileName.slice(0, originalFileName.lastIndexOf('.')),
+        '-' + size.name,
+        originalFileName.slice(originalFileName.lastIndexOf('.')),
+      ].join('')
+
+      const filePath = this.createFilePath(versionfileName, data.attributes.spaceId)
+
+      const S3Result = await this.sendFileToS3({
+        Key: filePath,
+        Bucket: config.s3.bucket,
+        ACL: 'public-read',
+        ContentType: data.file.mimetype,
+        Body: images[size.name],
+      })
+
+      versions[size.name] = S3Result.Location
+    }
+
+    return versions
+  }
+
+  private getUploadImageConfig(uploadType: UploadType): IUploadImageConfig | null {
+    for (const cnfg of UploadImageConfig) {
+      if (cnfg.type === uploadType) {
+        return cnfg
+      }
+    }
+
+    return null
+  }
+
+  sendFileToS3(params: S3.Types.PutObjectRequest): Promise<S3.ManagedUpload.SendData> {
     return new Promise((resolve, reject) => {
-      this.s3.upload(
-        {
-          Key: filePath,
-          Bucket: config.s3.bucket,
-          ACL: 'public-read',
-          ContentType: file.mimetype,
-          Body: fs.createReadStream(file.path),
-        },
-        (err, output) => {
-          if (err) {
-            reject(err)
-            return
-          }
-          resolve(output)
+      this.s3.upload(params, (err, output) => {
+        if (err) {
+          reject(err)
+          return
         }
-      )
+        resolve(output)
+      })
     })
   }
 
