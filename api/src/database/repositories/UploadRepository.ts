f148d5086f1343b584e8cbd4b3ffa82176d7fc89
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-26 01:20:44 +0200

    upload image versions

--- api/src/database/repositories/UploadRepository.ts
+++ api/src/database/repositories/UploadRepository.ts
@@ -2,4 +2,11 @@ import { EntityRepository, Repository } from 'typeorm'
 import { Upload } from '../entities/Upload'
 
 @EntityRepository(Upload)
-export class UploadRepository extends Repository<Upload> {}
+export class UploadRepository extends Repository<Upload> {
+  getByEntityId(entityId: number, entity: string): Promise<Upload | undefined> {
+    return this.createQueryBuilder('upload')
+      .where('upload.entityId = :entityId', { entityId })
+      .andWhere('upload.entity = :entity', { entity })
+      .getOne()
+  }
+}
--- api/src/types/upload.ts
+++ api/src/types/upload.ts
@@ -1,5 +1,51 @@
 export enum UploadType {
-  SpaceLogo = 'spaceLogo',
   UserAvatar = 'userAvatar',
+  SpaceLogo = 'spaceLogo',
   TaskAttachment = 'taskAttachment',
 }
+
+export const UploadUniqueTypes = [UploadType.UserAvatar, UploadType.SpaceLogo]
+
+export interface IUploadImageSize {
+  name: string
+  width: number
+  height?: number
+}
+
+export interface IUploadImageConfig {
+  type: UploadType
+  sizes: IUploadImageSize[]
+}
+
+export const UploadImageConfig: IUploadImageConfig[] = [
+  {
+    type: UploadType.UserAvatar,
+    sizes: [
+      {
+        name: 'small',
+        width: 28,
+        height: 28,
+      },
+      {
+        name: 'medium',
+        width: 96,
+        height: 96,
+      },
+    ],
+  },
+  {
+    type: UploadType.TaskAttachment,
+    sizes: [
+      {
+        name: 'small',
+        width: 80,
+        height: 64,
+      },
+      {
+        name: 'preview',
+        width: 1024,
+        height: null,
+      },
+    ],
+  },
+]
