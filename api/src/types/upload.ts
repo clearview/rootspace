82825c4d04be363690b5eaee8fd916d45bb7ef81
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-27 00:50:42 +0200

    uploads space logo, add check is file image, code improvments

--- api/src/types/upload.ts
+++ api/src/types/upload.ts
@@ -32,8 +32,6 @@ export interface IUploadVersions {
   [key: string]: IUploadVersion
 }
 
-export const UploadUniqueTypes = [UploadType.UserAvatar, UploadType.SpaceLogo]
-
 export interface IUploadImageSize {
   name: string
   width: number
@@ -44,36 +42,3 @@ export interface IUploadImageConfig {
   type: UploadType
   sizes: IUploadImageSize[]
 }
-
-export const UploadImageConfig: IUploadImageConfig[] = [
-  {
-    type: UploadType.UserAvatar,
-    sizes: [
-      {
-        name: 'small',
-        width: 28,
-        height: 28,
-      },
-      {
-        name: 'medium',
-        width: 96,
-        height: 96,
-      },
-    ],
-  },
-  {
-    type: UploadType.TaskAttachment,
-    sizes: [
-      {
-        name: 'small',
-        width: 80,
-        height: 64,
-      },
-      {
-        name: 'preview',
-        width: 1024,
-        height: null,
-      },
-    ],
-  },
-]
