a7532cd8a7c1b6e6ff71f6a5d3d3b21df6b08afd
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-26 23:21:32 +0200

    uploads rename mime to mimetype

--- api/src/database/migrations/1598302905134-UploadsRenameTypeToMimetype.ts
+++ api/src/database/migrations/1598302905134-UploadsRenameTypeToMimetype.ts
@@ -3,10 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class UploadsRenameTypeToMime1598302905134 implements MigrationInterface {
   name = 'UploadsRenameTypeToMime1598302905134'
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "uploads" RENAME COLUMN "type" TO "mime"`)
+    await queryRunner.query(`ALTER TABLE "uploads" RENAME COLUMN "type" TO "mimetype"`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "uploads" RENAME COLUMN "mime" TO "type"`)
+    await queryRunner.query(`ALTER TABLE "uploads" RENAME COLUMN "mimetype" TO "type"`)
   }
 }
