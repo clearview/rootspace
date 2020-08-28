85f615bd2b4ed14d6ce99ff79711799d306a20be
Mujo Kodro <mujo.kodro@gmail.com>
2020-08-29 00:24:21 +0200

    space logo

--- api/src/database/repositories/SpaceRepository.ts
+++ api/src/database/repositories/SpaceRepository.ts
@@ -1,19 +1,37 @@
-import { EntityRepository, Repository, UpdateResult } from 'typeorm'
+import { EntityRepository, Repository, UpdateResult, SelectQueryBuilder } from 'typeorm'
 import { UserToSpace } from '../entities/UserToSpace'
 import { Space } from '../entities/Space'
+import { Upload } from '../entities/Upload'
 
 @EntityRepository(Space)
 export class SpaceRepository extends Repository<Space> {
   getByUserId(userId: number) {
-    return this.createQueryBuilder('spaces')
+    const queryBuilder = this.createQueryBuilder('spaces')
       .innerJoin(UserToSpace, 'userToSpace', 'userToSpace.spaceId = spaces.id')
       .where('userToSpace.userId = :userId AND userToSpace.active = true', {
         userId,
       })
-      .getMany()
+
+    this.mapLogo(queryBuilder)
+
+    return queryBuilder.getMany()
   }
 
   updateCountMembers(spaceId: number, count: number): Promise<UpdateResult> {
     return this.update(spaceId, { countMembers: count })
+  }
+
+  private mapLogo(queryBuilder: SelectQueryBuilder<Space>): SelectQueryBuilder<Space> {
+    const alias = queryBuilder.alias
+
+    return queryBuilder.leftJoinAndMapOne(
+      alias + '.avatar',
+      Upload,
+      'upload',
+      'upload.entityId = ' + alias + '.id AND upload.entity = :entity',
+      {
+        entity: 'Space',
+      }
+    )
   }
 }
