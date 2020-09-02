ceb5a86285d96765e6f6c294bd8efa882f8f8b76
Mirza Eka <mirzaeka@gmail.com>
2020-09-02 15:34:31 +0700

    Fix TS Error and adding new Error Message

--- web/src/utils/error.ts
+++ web/src/utils/error.ts
@@ -13,32 +13,37 @@ type ValidationParser = {
 }
 
 export class ValidationError extends Error {
-  fields: string[];
+   fields: string[] = []
 
   static labels: ValidationLabel = {
     firstName: 'First Name',
     lastName: 'Last Name',
     email: 'Email',
     password: 'Password',
+    newPassword: 'New Password',
     password_confirmation: 'Password Confirmation' // eslint-disable-line
   }
 
   static parser: ValidationParser = {
     dbUnique: label => `${label} is already exist`,
-    required: label => `${label} is required`
+    required: label => `${label} is required`,
+    min: label => `${label} should contain at least 8 characters`,
+    compromisedPassword: label => `${label} is weak and can be easily guessed`
   }
 
   constructor (message: string, fields: ValidationField[]) {
-    super(message)
+    super(message.includes('invalid input syntax for type uuid') ? 'Invalid Token' : message)
 
-    this.fields = fields.map(
-      ({ field, validation }: ValidationField) => {
-        const label = ValidationError.labels[field]
-        const parse = ValidationError.parser[validation]
+    if (fields) {
+      this.fields = fields.map(
+        ({ field, validation }: ValidationField) => {
+          const label = ValidationError.labels[field]
+          const parse = ValidationError.parser[validation]
 
-        return parse(label)
-      }
-    )
+          return parse(label)
+        }
+      )
+    }
 
     this.stack = new Error().stack
   }
