df1affe727c11ca02d0bf8d7508594b60b868c0c
Mujo Kodro <mujo.kodro@gmail.com>
2020-09-03 17:41:18 +0200

    docContent uplaod type

--- api/src/types/upload.ts
+++ api/src/types/upload.ts
@@ -2,6 +2,14 @@ export enum UploadType {
   UserAvatar = 'userAvatar',
   SpaceLogo = 'spaceLogo',
   TaskAttachment = 'taskAttachment',
+  DocContent = 'docContent',
+}
+
+export enum UploadEntity {
+  User = 'User',
+  Space = 'Space',
+  Task = 'Task',
+  Doc = 'Doc',
 }
 
 export interface IUploadImageSize {
