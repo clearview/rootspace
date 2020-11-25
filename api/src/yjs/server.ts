f79c4ee21203012603e8c0cf527821152c53903c
Mujo Kodro <mujo.kodro@gmail.com>
2020-11-25 21:11:36 +0100

    yjs socket authentication

--- api/src/yjs/server.ts
+++ api/src/yjs/server.ts
@@ -1,8 +1,11 @@
-import * as http from 'http'
+import * as Http from 'http'
 import * as WebSocket from 'ws'
 import * as wsUtils from 'y-websocket/bin/utils.js'
 import * as Y from 'yjs'
+import jwt from 'jsonwebtoken'
+import { config } from 'node-config-ts'
 import { ServiceFactory } from '../services/factory/ServiceFactory'
+import { UserService } from '../services'
 
 const port = 6001
 
@@ -38,31 +41,79 @@ const persistence = {
   },
 }
 
+const authenticate = async (conn: any, token: string): Promise<boolean> => {
+  try {
+    const decoded: any = jwt.verify(token, config.jwt.accessToken.secretKey)
+    const userId = decoded.id
+
+    if (typeof userId !== 'number') {
+      return false
+    }
+
+    const user = await UserService.getInstance().getUserById(userId)
+
+    if (!user) {
+      return false
+    }
+
+    conn.user = user
+
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
 wsUtils.setPersistence(persistence)
 
 export default class YjsServer {
-  httpServer: http.Server
-  wss: any
+  httpServer: Http.Server
+  wss: WebSocket.Server
 
   constructor() {
-    this.httpServer = http.createServer((request, response) => {
+    this.httpServer = Http.createServer((request, response) => {
       response.writeHead(200, { 'Content-Type': 'text/plain' })
       response.end('Okay')
     })
 
     this.wss = new WebSocket.Server({ noServer: true })
-    this.wss.on('connection', wsUtils.setupWSConnection)
 
     this.httpServer.on('upgrade', (request, socket, head) => {
-      // You may check auth of request here..
-      /**
-       * @param {any} ws
-       */
-      const handleAuth = (ws) => {
-        this.wss.emit('connection', ws, request)
-      }
-      this.wss.handleUpgrade(request, socket, head, handleAuth)
+      this.wss.handleUpgrade(request, socket, head, this.handleAuth)
     })
+
+    this.wss.on('connection', (conn: WebSocket, req: Http.IncomingMessage, {} = {}) => {
+      conn.on('message', async (message) => {
+        await this.onMessage(conn, req, message)
+      })
+    })
+  }
+
+  private handleAuth = (ws: WebSocket, request: Http.IncomingMessage) => {
+    this.wss.emit('connection', ws, request)
+  }
+
+  private onMessage = async (conn: WebSocket, req: Http.IncomingMessage, message: any) => {
+    const docName = req.url.slice(1)
+
+    if (typeof message === 'string') {
+      const authenticated = await authenticate(conn, message)
+
+      if (authenticated === false) {
+        conn.send('unauthenticated')
+        conn.close()
+        return
+      }
+
+      wsUtils.setupWSConnection(conn, req, docName)
+      conn.send('authenticated')
+      return
+    }
+
+    if (!(conn as any).user) {
+      conn.send('unauthenticated')
+      conn.close()
+    }
   }
 
   listen() {
