8c95c5d8448bec76e8482251a530376440d1850e
Adi Utama <neogatotkaca@gmail.com>
2021-02-02 13:30:04 +0700

    Linking activity on profile modal to entity page

--- web/src/components/Activities.vue
+++ web/src/components/Activities.vue
@@ -1,36 +1,63 @@
 <template>
   <div class="activities">
-    <div class="activity" v-for="(item, index) in value" :key="index">
-      <div class="activity-content" v-html="formatActivity(item)"></div>
-      <div class="activity-time">{{item.createdAt | elapsedTime}}</div>
+    <div
+      v-for="(item, index) in list"
+      :key="index"
+      :class="{
+        'activity': 'true',
+        'is-clickable': item.link
+      }"
+      @click="open(item)"
+    >
+      <div class="activity-content" v-html="item.content"></div>
+      <div class="activity-time">{{item.time}}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+import { computed, defineComponent, PropType } from '@vue/composition-api'
+import { Location } from 'vue-router'
 import { formatDistanceToNowStrict, parseISO } from 'date-fns'
 import { ActivityResource } from '@/types/resource'
 import { textFormat } from '@/utils/textFormat'
+import { getEntityLink } from '@/utils/activity'
 
-@Component({
-  filters: {
-    elapsedTime (date: string) {
-      return formatDistanceToNowStrict(parseISO(date), { addSuffix: true })
-    },
-    formatActivity (activity: ActivityResource) {
-      return textFormat(activity).text
+interface ActivityItem {
+  link: Location | string;
+  content: string;
+  time: string;
+}
+
+export default defineComponent({
+  props: {
+    value: {
+      type: Array as PropType<ActivityResource[]>,
+      default: () => ([])
+    }
+  },
+  setup (props, { emit, root }) {
+    const list = computed<ActivityItem[]>(() => props.value.map(
+      (item) => ({
+        link: getEntityLink(item),
+        content: textFormat(item).text,
+        time: formatDistanceToNowStrict(parseISO(item.createdAt), { addSuffix: true })
+      })
+    ))
+
+    const open = (item: ActivityItem) => {
+      if (!item.link) return
+
+      emit('click', item)
+      root.$router.push(item.link)
+    }
+
+    return {
+      list,
+      open
     }
   }
 })
-export default class Activities extends Vue {
-  @Prop({ type: Array })
-  readonly value!: ActivityResource[]
-
-  formatActivity (activity: ActivityResource) {
-    return textFormat(activity).text
-  }
-}
 </script>
 
 <style lang="postcss" scoped>
@@ -44,6 +71,13 @@ export default class Activities extends Vue {
   & + & {
     border-top: 1px solid #EDEFF3;
   }
-}
 
+  &:hover {
+    background-color: #F8F9FD;
+  }
+
+  &.is-clickable {
+    @apply cursor-pointer;
+  }
+}
 </style>
