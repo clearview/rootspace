760294b4a3661284110c60b3c846c7ca475de45a
fudyartanto <arfan.fudyartanto@gmail.com>
2021-03-31 18:48:00 +0700

    Fix broken style in the Space setting page

--- web/src/components/form/FormSpace.vue
+++ web/src/components/form/FormSpace.vue
@@ -29,7 +29,7 @@
       </template>
     </v-field>
 
-    <v-field label="Team" name="team" has-addon has-icon-left>
+    <v-field label="Team" name="team" id="team-field" has-addon has-icon-left>
       <legacy-icon class="icon is-left" name="plus" size="1.3em" viewbox="32"/>
       <input
         type="email"
