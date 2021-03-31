be8eba20fd2e8e4179f9dfec3e5691a861f45f43
Aid Arslanagic <aid@clearview.team>
2021-03-31 20:16:13 +0200

    Fix wrong import

--- api/tests/helpers/createUser.ts
+++ api/tests/helpers/createUser.ts
@@ -1,7 +1,7 @@
 import * as faker from 'faker'
 import { User } from '../../src/database/entities/User'
 import { UserService } from '../../src/services'
-import { UserAuthProvider } from '../../src/values/user/UserAuthProvider'
+import { UserAuthProvider } from '../../src/types/user'
 
 async function createUser(email: string, password: string): Promise<User> {
     return UserService.getInstance().signup({
@@ -9,7 +9,7 @@ async function createUser(email: string, password: string): Promise<User> {
         lastName: faker.name.lastName(),
         email,
         password,
-        password_confirmation: password,
+        passwordConfirmation: password,
         authProvider: UserAuthProvider.LOCAL,
         active: true
     }, false)
