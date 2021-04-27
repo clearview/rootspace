e911a9137ed022e03745b9280e312d99ec474fb9
Mujo Kodro <mujo.kodro@gmail.com>
2021-04-28 00:35:29 +0200

    remove WsHeadersMiddleware

--- api/src/middleware/WsHeadersMiddleware.ts
+++ api/src/middleware/WsHeadersMiddleware.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express'
 
 export function removeWsHeaders() {
   return (req: Request, res: Response) => {
-    // res.removeHeader('Server')
+    res.removeHeader('Server')
   }
 }
