fa825097d69efacf5051c5977f7f52297b089b38
Januar Fonti <fonti@clearview.team>
2021-05-17 16:08:20 +0700

    Add temp progress file upload on list view

--- web/src/views/Files/FileItem.vue
+++ web/src/views/Files/FileItem.vue
@@ -8,6 +8,24 @@
           @deleted="refreshParent"
         />
        </div>
+       <div class="temp-file-item list-view" v-if="isUploading">
+        <div class="progress-wrapper">
+          <radial-progress-bar
+            :diameter="70"
+            :completedSteps="tempFile.progress"
+            innerStrokeColor="#dee2ee"
+            startColor="#8cd5ff"
+            stopColor="#8cd5ff"
+            :strokeWidth="5"
+            :innerStrokeWidth="5"
+            :totalSteps="100" >
+            <span>{{ tempFile.progress }} %</span>
+          </radial-progress-bar>
+        </div>
+        <div class="content">
+          {{ tempFile.name }}
+        </div>
+      </div>
     </div>
     <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4" v-else>
       <div v-for="(file, index) in item.uploads" :key="index">
@@ -17,7 +35,7 @@
           @deleted="refreshParent"
         />
       </div>
-      <div class="file-item" v-if="isUploading">
+      <div class="temp-file-item grid-view" v-if="isUploading">
         <div class="progress-wrapper">
           <radial-progress-bar
             :diameter="111"
@@ -80,17 +98,33 @@ export default class FileItem extends Vue {
 </script>
 
 <style lang="postcss" scoped>
-.file-item {
-  @apply flex flex-col;
-  border: 1px solid #DEE2EE;
+.temp-file-item {
+  @apply flex;
   border-radius: 4px;
-  .progress-wrapper {
-    @apply flex flex-wrap justify-center content-center w-full h-full;
+  &.grid-view {
+    @apply flex-col;
+    border: 1px solid #DEE2EE;
+    .progress-wrapper {
+      @apply flex flex-wrap justify-center content-center w-full h-full;
+    }
+    .content {
+      @apply p-5 truncate;
+      font-size: 1rem;
+      font-weight: bold;
+    }
   }
-  .content {
-    @apply p-5 truncate;
-    font-size: 1rem;
-    font-weight: bold;
+  &.list-view {
+    @apply flex-row;
+    border-bottom: 1px solid #F8F8FB;
+    .progress-wrapper {
+      @apply flex flex-wrap justify-center content-center p-4;
+    }
+    .content {
+      @apply flex w-full truncate;
+      align-items: center;
+      font-size: 1rem;
+      font-weight: bold;
+    }
   }
 }
 </style>
