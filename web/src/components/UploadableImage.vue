511a48f3e34bdd28ff227166fc4217d81b6c6bef
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-12-09 07:27:45 +0700

    fix: fixing issue can't upload after remove logo

--- web/src/components/UploadableImage.vue
+++ web/src/components/UploadableImage.vue
@@ -118,6 +118,7 @@ export default class UploadableImage extends Vue {
     await store.dispatch('auth/whoami', { updateSpace: true })
 
     this.isUploadingImage = false
+    this.fakeImage = ''
   }
 }
 </script>
