9ee5f90a27ed93c62f9e156d0ae4ea249652fa92
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-12-14 20:24:02 +0700

    feat: create an handler if refresh token has expired

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -11,6 +11,19 @@ api.interceptors.response.use((response) => {
   const originalConfig = error.config
 
   if (originalConfig.url !== '/auth/signin' && error.response) {
+    // Refresh Token was expired
+    if (originalConfig.url === '/users/token' && error?.response?.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true
+
+      try {
+        await store.dispatch('auth/signout')
+
+        return api(originalConfig)
+      } catch (_error) {
+        return Promise.reject(_error)
+      }
+    }
+
     // Access Token was expired
     if (error.response.status === 401 && !originalConfig._retry) {
       originalConfig._retry = true
