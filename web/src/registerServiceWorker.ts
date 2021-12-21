e8acdd9de462e158e7038af8307e7c1c5ec3e1e4
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-12-21 20:36:57 +0700

    fix: comment all console logs on registerServiceWorker

--- web/src/registerServiceWorker.ts
+++ web/src/registerServiceWorker.ts
@@ -5,31 +5,31 @@ import { register } from 'register-service-worker'
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
+      // console.log(
+      //   'App is being served from cache by a service worker.\n' +
+      //   'For more details, visit https://goo.gl/AFskqB'
+      // )
     },
     registered (registration) {
-      console.log('Service worker has been registered.')
+      // console.log('Service worker has been registered.')
       setInterval(() => {
         registration.update()
       }, 1000 * 60) // will be check every minute
     },
     cached () {
-      console.log('Content has been cached for offline use.')
+      // console.log('Content has been cached for offline use.')
     },
     updatefound () {
-      console.log('New content is downloading.')
+      // console.log('New content is downloading.')
     },
     updated (registration) {
-      console.log('New content is available; please refresh.')
+      // console.log('New content is available; please refresh.')
       document.dispatchEvent(
         new CustomEvent('swUpdated', { detail: registration })
       )
     },
     offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+      // console.log('No internet connection found. App is running in offline mode.')
     },
     error (error) {
       console.error('Error during service worker registration:', error)
