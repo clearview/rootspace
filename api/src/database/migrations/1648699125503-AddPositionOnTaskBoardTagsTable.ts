0ad048993879fe670108232a5d7baec225ac24fe
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2022-03-31 11:12:01 +0700

    feat: add a new migration for adding position column

---
+++ api/src/database/migrations/1648699125503-AddPositionOnTaskBoardTagsTable.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddPositionOnTaskBoardTagsTable1648699125503 implements MigrationInterface {
+    name = 'AddPositionOnTaskBoardTagsTable1648699125503'
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "task_board_tags" ADD COLUMN "position" integer NULL DEFAULT 0`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "task_board_tags" DROP COLUMN "position"`)
+    }
+
+}
