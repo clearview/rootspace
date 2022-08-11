446e489caaca55067b0db3b81b643545ac6f3636
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2022-08-11 19:17:48 +0200

    Web, fix Dockerfile

--- infra/docker/web/entrypoint.sh
+++ infra/docker/web/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-cd /srv/
-ls .
-echo "Installing packages... $(yarn install)"
-echo "Building web...  $(NODE_OPTIONS="--max-old-space-size=200" VUE_MEMORY_LIMIT=200 yarn build)"
+pwd && ls && echo "Starting the command from entrypoint.sh"
 exec "$@"
