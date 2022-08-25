d89741b33b41be54736f1a3eb12f2138a1777c88
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2022-08-25 21:51:56 +0200

    fix

--- infra/docker/api/entrypoint.sh
+++ infra/docker/api/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 cd /srv && \
 echo "Writing GOOGLE_CLIENT_ID to .env file... $(echo "GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID" >> .env)"
+echo "Writing GOOGLE_CLIENT_SECRET to .env file... $(echo "GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET" >> .env)"
 echo "Writing POSTGRES to .env file... $(echo "POSTGRES=postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" >> .env)"
 echo "Writing REDIS_HOST to .env file... $(echo "REDIS_HOST=$REDIS_HOST" >> .env)"
 echo "Writing REDIS_PORT to .env file... $(echo "REDIS_PORT=$REDIS_PORT" >> .env)"
