277c00ac99a0df0713216b310cf69183a8cf9910
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2022-08-31 12:00:09 +0200

    Del unused infra files

--- infra/kubernetes/create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl apply -f namespace.yaml
-kubectl apply -f secret.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
-kubectl apply -f ingress.yaml
-# Get
-kubectl get pods -n flow
--- infra/kubernetes/helm/nginx_ingress.sh
+++
@@ -1,4 +0,0 @@
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo update
-helm upgrade --install nginx-ingress ingress-nginx/ingress-nginx --namespace kube-system
