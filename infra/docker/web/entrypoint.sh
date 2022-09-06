e153179acefcd2317c6de1753d555d641fc66e13
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2022-09-06 22:20:06 +0200

    web, test api endpoint on startup

--- infra/docker/web/entrypoint.sh
+++ infra/docker/web/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+echo "Check connection to API_ENDPOINT... $(nc -zv $API_ENDPOINT 443)"
+cat .env
 pwd && ls && ls web && echo "Starting the command from entrypoint.sh"
 exec "$@"
