ec3f2e36f2c94b51772cb7e65a298430169eb5c0
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2022-09-06 22:54:45 +0200

    fix

--- infra/docker/web/entrypoint.sh
+++ infra/docker/web/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Check connection to API_ENDPOINT... $(nc -zv $API_ENDPOINT 443)"
-cat .env
-pwd && ls && ls web && echo "Starting the command from entrypoint.sh"
+cat cat web/.env
+echo "Starting the command from entrypoint.sh"
 exec "$@"
